02c6551f81aa7b06be1843e65cbe7b340c8687eb
Abukari Einus <blackmartyr00@gmail.com>
2025-02-19 09:52:50 +0000

    initial

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::fs;
+use std::env;
+fn main() {
+    let name_input = env::args().nth(1).unwrap();
+    let file_contents = fs::read_to_string("roster.txt").unwrap();
+
+    for line in file_contents.lines() {
+        if name_input == line {
+            println!("{} is part of the roster", name_input);
+            return;
+        } else {
+            println!("{} is not part of the roster", name_input);  
+        }
+    }
+}
